4ed5104f10e634989f33f2d0265b3807d10cadba
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2026-05-15 16:19:34 -0300

    Initial Commit

---
+++ Luci/LuciApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuciApp.swift
+//  Luci
+//
+//  Created by João Duque Nardelli Wandermuren on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LuciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

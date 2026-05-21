be948e74501293e59f7a852c9fd9ebf5a478a711
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2026-05-21 18:43:37 -0300

    Arrumar roteador

--- Luci/LuciApp.swift
+++ Luci/LuciApp.swift
@@ -9,38 +9,9 @@ import SwiftUI
 
 @main
 struct LuciApp: App {
-    
-    @StateObject private var router = Router()
-    
     var body: some Scene {
         WindowGroup {
-            NavigationStack(path: $router.path) {
-                
-//                SplashView()
-                
-                ContentView()
-                
-                .navigationDestination(for: Rotas.self) { destino in
-                    switch destino {
-                    case .splash:
-//                        SplashView()
-                        MisturaView()
-                            .navigationTitle("Mistura")
-                            .navigationBarBackButtonHidden(true)
-                    case .mistura:
-                        MisturaView()
-                    case .transicao:
-//                        TransicaoView()
-                        MisturaView()
-                    case .resultado:
-//                        ResultadoView()
-                        MisturaView()
-                    case .saibamais:
-//                        NoticiasView()
-                        MisturaView()
-                    }
-                }
-            }
+            RouterView()
         }
     }
 }
--- Luci/Router.swift
+++ Luci/Router.swift
@@ -12,3 +12,39 @@ import Combine
 class Router: ObservableObject {
     @Published var path = NavigationPath()
 }
+
+
+// Rotacionador
+struct RouterView: View {
+    @StateObject private var router = Router()
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            
+            //QUando criar SplashView, tirar o comentário dessa linha, fazer o msm pras outras telas
+//                SplashView()
+            
+            ContentView()
+            
+            .navigationDestination(for: Rotas.self) { destino in
+                switch destino {
+                case .splash:
+//                        SplashView()
+                    MisturaView()
+                case .mistura:
+                    MisturaView()
+                case .transicao:
+//                        TransicaoView()
+                    MisturaView()
+                case .resultado:
+//                        ResultadoView()
+                    MisturaView()
+                case .saibamais:
+//                        NoticiasView()
+                    MisturaView()
+                }
+            }
+        }
+    }
+    
+}

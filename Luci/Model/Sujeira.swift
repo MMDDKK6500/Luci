36a7377bd614c0d1d046af6ddfe1ea5204b0af3c
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2026-05-25 14:52:30 -0300

    Ver se funciona

--- Luci/Model/Sujeira.swift
+++ Luci/Model/Sujeira.swift
@@ -10,12 +10,28 @@ import Foundation
 struct Sujeira: Hashable {
     let nome: String
     let receita: String
+    
+    let troca: Bool
+    
+    var trocaTipo: [[ProdutoTipos]]?
 }
 
 
 enum Sujeiras {
-    static let tintaCaneta = Sujeira(
-        nome: "Álcool",
-        receita: "Umedecer um pano com álcool e esfregar na área com tinta de caneta"
+    static let poeira = Sujeira(
+        nome: "Poeira",
+        receita: "Umedecer um pano com álcool e esfregar na área com tinta de caneta",
+        troca: false
+    )
+    static let mofo = Sujeira(
+        nome: "Mofo",
+        receita: "Como tirar mofo",
+        troca: false
+    )
+    static let gordura = Sujeira(
+        nome: "Gordura",
+        receita: "Como tirar gordura",
+        troca: true,
+        trocaTipo: [[.neutro, .alcalino]]
     )
 }
--- Luci/Model/Superficie.swift
+++ Luci/Model/Superficie.swift
@@ -20,7 +20,7 @@ enum Superficies {
     static let porcelanato = Superficie(
         nome: "Porcelanato",
         produtos: [Produtos.agua, Produtos.detergente],
-        produtosTipos: [.morna, .neutro],
+        produtosTipos: [.morna, .alcalino],
         quantidadeProdutos: [
             ["pouca água", "media agua", "muita agua"],
             ["pouco detergente", "medio detergente", "muito detergente"]
@@ -44,6 +44,42 @@ enum Superficies {
             ["pouco detergente", "medio detergente", "muito detergente"]
         ]
     )
+}
+
+func resultar(superficie: Superficie, nivelSujeira: Int, adicional: Sujeira) -> String {
+    var resultado = Receitas.misturarBalde.rawValue
+    
+    // Determinar qual tipo de produto usar, duranto o for!!!
+    
+    for i in 0...superficie.quantidadeProdutos.count - 1 {
+        if let range = resultado.range(of:"{QUANTIDADE}") {
+            resultado = resultado.replacingCharacters(in:range, with: superficie.quantidadeProdutos[i][nivelSujeira])
+        }
+        if let range = resultado.range(of:"{PRODUTO}") {
+            let produto = superficie.produtos[i]
+            
+            var tipos = superficie.produtosTipos
+            
+//            if adicional.troca {
+//                
+//                for tipo in tipos {
+//                    
+//                    for adicional.trocaTipo in adicional.trocaTipo {
+//                        
+//                    }
+//                }
+//                
+//            }
+            
+            resultado = resultado.replacingCharacters(in:range, with: produto.nome + " " + superficie.produtosTipos[i].rawValue)
+        }
+    }
+    
+    
+    
+    resultado += "\n" + adicional.receita
+    
+    return resultado
 }
 
 // porcelanagto = detergente, normalmente neutro
